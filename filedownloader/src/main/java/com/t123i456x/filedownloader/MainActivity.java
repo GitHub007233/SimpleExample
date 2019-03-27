f965586c2e55f4191a4a3072687155b0a5eb2a19
安卓开发者 <t123i456x@outlook.com>
2019-03-27 16:50:17 +0800

    更新文件！

---
+++ filedownloader/src/main/java/com/t123i456x/filedownloader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.t123i456x.filedownloader;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
